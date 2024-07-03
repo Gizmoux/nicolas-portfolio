26b46a5c102abcff8dc08e55c09cbae299d6ecda
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-03 10:01:05 +0200

    add font caption in Hero

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -6,18 +6,19 @@ import Image from 'next/image';
 const Hero = () => {
 	return (
 		<Section className="flex max-md:flex-col items-start">
-			<div className="flex-1"></div>
-			<h2>NIcolas de Riemacker</h2>
-			<h3>React Developper</h3>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ut,
-				voluptatem perferendis, odit nesciunt dicta ipsa quibusdam illo ratione
-			</p>
-			<div className="flex-2">
+			<div className="flex-[2]">
+				<h2 className="font-caption text-3xl">NIcolas de Riemacker</h2>
+				<h3>React Developper</h3>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ut,
+					voluptatem perferendis, odit nesciunt dicta ipsa quibusdam illo
+					ratione
+				</p>
+			</div>
+			<div className="flex-1">
 				<Image
-					width={200}
-					height={200}
 					src={profilPicture}
+					className="rounded-xl w-full h-auto max-w-xs"
 					alt="Profil Picture"
 				/>
 			</div>
