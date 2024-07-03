6fe4b5d7aa1c30aa94020bbf9f1633a085da5ffd
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-03 18:15:15 +0200

    Add Code component

---
+++ app/_components/Code.tsx
@@ -0,0 +1,16 @@
+import { cn } from '@/lib/utils';
+import { ComponentPropsWithoutRef } from 'react';
+export const Code = ({
+	className,
+	...props
+}: ComponentPropsWithoutRef<'span'>) => {
+	return (
+		<span
+			className={cn(
+				'bg-accent/30 font-mono hover:bg-accent/50 transition-colors border boder-accent p-1 rounded-sm text-primary',
+				className
+			)}
+			{...props}
+		/>
+	);
+};
