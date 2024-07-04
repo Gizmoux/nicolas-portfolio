9d5a19edb72b1821b1b8ada25568a7dcd452a2f7
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-04 16:07:13 +0200

    Add Hero

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -2,23 +2,38 @@ import React from 'react';
 import Section from './Section';
 import profilPicture from '../_components/assets/Images/profil-pic.jpg';
 import Image from 'next/image';
+import { Code } from './Code';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import GithubIcon from './icons/GithubIcon';
+import NextIcon from './icons/NextIcon';
 
 const Hero = () => {
 	return (
-		<Section className="flex max-md:flex-col items-start">
-			<div className="flex-[2]">
-				<h2 className="font-caption text-3xl">NIcolas de Riemacker</h2>
-				<h3>React Developper</h3>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad ut,
-					voluptatem perferendis, odit nesciunt dicta ipsa quibusdam illo
-					ratione
+		<Section className="flex max-md:flex-col items-start gap-10">
+			<div className="flex-[3] w-full flex flex-col gap-4">
+				<h2 className="font-caption font-bold text-4xl text-primary">
+					Nicolas de Riemacker
+				</h2>
+				<h3 className="font-caption text-3xl">Développeur React</h3>
+				<p className="text-base">
+					Vous pouvez voir mes projets sur
+					<Link href="http://github.com/Gizmoux">
+						<Code className="inline-flex items-center gap-2 mb-2">
+							<GithubIcon size={16} className="inline" />
+							Github
+						</Code>
+					</Link>
+					, codés avec certaines technologies comme
+					<Code className="inline-flex items-center gap-2 ml-4">
+						<NextIcon size={18} className="inline" /> Next.js
+					</Code>
 				</p>
 			</div>
-			<div className="flex-1">
+			<div className="flex-[2] max-md:m-auto ml-auto">
 				<Image
 					src={profilPicture}
-					className="rounded-xl w-full h-auto max-w-xs"
+					className="rounded-xl w-40 h-auto max-w-xs"
 					alt="Profil Picture"
 				/>
 			</div>
