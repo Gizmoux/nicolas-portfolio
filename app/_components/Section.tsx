ac3e7bf909758694fe4638b0e10af9d89439b988
Nicolas <nicolas.deriemacker@hotmail.fr>
2024-07-04 16:09:05 +0200

    Add Section component

--- app/_components/Section.tsx
+++ app/_components/Section.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from 'react';
 
 const Section = (props: PropsWithChildren<{ className?: string }>) => {
 	return (
-		<section className={cn('max-w-3xl px-4 m-auto', props.className)}>
+		<section className={cn('max-w-4xl px-4 m-auto', props.className)}>
 			{props.children}
 		</section>
 	);
