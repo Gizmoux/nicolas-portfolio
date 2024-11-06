003700838280fbe27081a67c9b8465bbc5b5a617
Gizmoux <nicolas.deriemacker@hotmail.fr>
2024-11-06 13:38:40 +0100

    ✅ Update sideproject component

--- app/_components/SideProject.tsx
+++ app/_components/SideProject.tsx
@@ -1,54 +1,54 @@
-'use client';
-import Link from 'next/link';
-import { LucideIcon, Drama, Rss, Weight, Bitcoin } from 'lucide-react';
+"use client";
+import Link from "next/link";
+import { LucideIcon, Drama, Rss, DollarSign, Bitcoin } from "lucide-react";
 
 export type SideProjectProps = {
-	Logo: LucideIcon;
-	title: string;
-	description: string;
-	url: string;
+  Logo: LucideIcon;
+  title: string;
+  description: string;
+  url: string;
 };
 
 export const SIDE_PROJECTS: SideProjectProps[] = [
-	{
-		Logo: Drama,
-		title: 'Marvel-App',
-		description: 'Une plateforme pour vos persos Marvel préférés',
-		url: '/',
-	},
-	{
-		Logo: Bitcoin,
-		title: 'Argent Bank',
-		description: 'Authentification et accès à son compte personnel',
-		url: '/',
-	},
-	{
-		Logo: Rss,
-		title: 'Vinted-App',
-		description: 'Clone de Vinted',
-		url: '/',
-	},
-	{
-		Logo: Weight,
-		title: 'Projet 14',
-		description: 'Changer de librairie jQuerie vers React',
-		url: '/',
-	},
+  {
+    Logo: Drama,
+    title: "Marvel-App",
+    description: "Une plateforme pour vos persos Marvel préférés",
+    url: "https://gitlab.com/gizmoux-marvel/marvel-app",
+  },
+  {
+    Logo: Bitcoin,
+    title: "Argent Bank",
+    description: "Authentification et accès à son compte personnel",
+    url: "https://github.com/Gizmoux/wealth-health",
+  },
+  {
+    Logo: Rss,
+    title: "Vinted-App",
+    description: "Clone de Vinted",
+    url: "/",
+  },
+  {
+    Logo: DollarSign,
+    title: "Billed",
+    description: "Testing with Jest",
+    url: "https://github.com/Gizmoux/P9---Billed",
+  },
 ];
 
 export const SideProject = (props: SideProjectProps) => {
-	return (
-		<Link
-			href={props.url}
-			className="inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-1 rounded"
-		>
-			<span className="bg-accent text-accent-foreground p-3 rounded-sm">
-				<props.Logo size={16} />
-			</span>
-			<div>
-				<p className="text-lg font-semibold">{props.title}</p>
-				<p className="text-lg text-muted-foreground">{props.description}</p>
-			</div>
-		</Link>
-	);
+  return (
+    <Link
+      href={props.url}
+      className="inline-flex items-center gap-4 hover:bg-accent/50 transition-colors p-1 rounded"
+    >
+      <span className="bg-accent text-accent-foreground p-3 rounded-sm">
+        <props.Logo size={16} />
+      </span>
+      <div>
+        <p className="text-lg font-semibold">{props.title}</p>
+        <p className="text-lg text-muted-foreground">{props.description}</p>
+      </div>
+    </Link>
+  );
 };
